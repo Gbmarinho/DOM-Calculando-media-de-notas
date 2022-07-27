680cb87daf64102a2f52b5fe739c5f5cced04734
Gabriel Marinho <gsmarinho@id.uff.br>
2022-07-27 17:18:32 -0300

    feat(script):Terminando scipt de media e notas

--- script.js
+++ script.js
@@ -3,21 +3,27 @@
 // }
 var notas = []
 var somaNotas = 0
-var soma = 1
+var soma = 0
 const btn = document.querySelector("#btn")
 const textarea = document.querySelector("#area")
 const input = document.querySelector(".nota")
+const resultado = document.querySelector("#resultado")
 btn.addEventListener("click", () => {
     notas.push(input.value)
+    soma = soma + 1
     textarea.value = `${textarea.value}\nA nota ${soma} foi: ${input.value}`
     input.value = ""
-    soma = soma + 1
 })
-for(j of notas){
-    somaNotas = somaNotas + j
-}
+
 const btn2 = document.querySelector("#btn2")
 btn2.addEventListener("click", () => {
-
-    console.log(print[0])
+    for(j of notas){
+        somaNotas = somaNotas + parseFloat(j.replace(',', '.'), 10); 
+    }
+    var media = somaNotas/soma
+    resultado.innerText = media.toFixed(2)
+    notas = []
+    media = 0
+    soma = 0
+    somaNotas = 0
 })
