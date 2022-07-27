206db27eab59357a6568f9f26c71c6034609eff5
Gabriel Marinho <gsmarinho@id.uff.br>
2022-07-27 20:40:26 -0300

    fix:erro na caixa

--- script.js
+++ script.js
@@ -7,17 +7,16 @@ const input = document.querySelector(".nota")
 const resultado = document.querySelector("#resultado")
 btn.addEventListener("click", () => {
     var confirm = 0     
-    confirm = parseFloat(input.value.replace(',', '.'))     
-    console.log(confirm)
-    if((0 <= confirm)&&(confirm<=10)){             
+    confirm = parseFloat(input.value.replace(',', '.'))
+    var h = isNaN(input.value.replace(',', '.'))     
+    if((0 <= confirm)&&(confirm<=10)&&(h != true)){             
         notas.push(input.value) 
-        soma = soma + 1        
-        console.log(notas)         
+        soma = soma + 1                
         textarea.value = `${textarea.value}A nota ${soma} foi: ${input.value}\n`      
         input.value = ""           
     }
     else {
-        alert("Por favor, digite uma nota valida!!!")     
+        alert("Por favor insire uma nota!!!")     
     }
 })
 
